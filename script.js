8f439fb29931d7525b3888409ff37d7af1ab5ca3
will-yell <40315545+willyell@users.noreply.github.com>
2024-11-24 17:09:52 +0200

    Update and rename scripts.js to script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// JavaScript to shrink the header on scroll
+document.addEventListener("scroll", () => {
+    const header = document.querySelector("header");
+    if (window.scrollY > 50) {
+        header.classList.add("shrink");
+    } else {
+        header.classList.remove("shrink");
+    }
+});
